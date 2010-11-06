304a261079bdc1911667af0699014b01f11d9ed2
MPS <genni951@gmail.com>
2010-11-06 12:48:45 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'd168fe3a7608184878907bf630964e3ce714834365b5afa523ca9018a40b556e96ea167b9cd492d05e555ca2440c24c46fc44af2d95f63fdabdc8efd56ed8ffc'
